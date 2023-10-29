30c14f84f74412f862f6faa630a21237819b7524
Dokyun Kim <donny5557@gmail.com>
2023-10-29 19:07:19 -0400

    implement light scheduling

--- schedule/src/main.cpp
+++ schedule/src/main.cpp
@@ -2,6 +2,8 @@
 #include "time.h"
 #include <Arduino.h>
 
+#define ledPin 45
+
 const char *ssid = "OLIN-DEVICES";
 const char *password = "Engineering4Every1!";
 
@@ -24,21 +26,54 @@ void setup()
   }
   Serial.println(" CONNECTED");
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+  pinMode(ledPin, OUTPUT);
 }
 
-void printLocalTime()
+void lightSchedule(int startHr, int startMin, int durationHr, int durationMin)
 {
+  time_t now;
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo))
   {
     Serial.println("Failed to obtain time");
     return;
   }
+  now = mktime(&timeinfo);
   Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+
+  // START TIME
+  struct tm startTime = timeinfo;
+  startTime.tm_hour = startHr;
+  startTime.tm_min = startMin;
+  time_t startTimestamp = mktime(&startTime);
+  String startDisp = "Start at: " + String(startTime.tm_hour) + ":" + String(startTime.tm_min) + ":" + String(startTime.tm_sec);
+  Serial.println(startDisp);
+
+  // END TIME
+  struct tm endTime = startTime;
+  endTime.tm_hour += durationHr;
+  endTime.tm_min += durationMin;
+  time_t endTimestamp = mktime(&endTime);
+  String endDisp = "End at: " + String(endTime.tm_hour) + ":" + String(endTime.tm_min) + ":" + String(endTime.tm_sec);
+  Serial.println(endDisp);
+
+  bool lightOn = (now >= startTimestamp && now < endTimestamp);
+
+  if (lightOn)
+  {
+    Serial.println("ON");
+    neopixelWrite(ledPin, 100, 150, 30);
+  }
+  else
+  {
+    Serial.println("OFF");
+    neopixelWrite(ledPin, 0, 0, 0);
+  }
 }
 
 void loop()
 {
   delay(1000);
-  printLocalTime();
+  lightSchedule(19, 04, 0, 1);
 }
