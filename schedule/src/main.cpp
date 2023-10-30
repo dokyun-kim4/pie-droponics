5690a4d83053b17fe06d043368fc87f135a4a4ff
Dokyun Kim <donny5557@gmail.com>
2023-10-30 19:31:13 -0400

    pump control

--- schedule/src/main.cpp
+++ schedule/src/main.cpp
@@ -1,13 +1,53 @@
 #include <Arduino.h>
+#define pumpPin 10
+
+uint32_t prevMillis;
+int onDuration = 2000;  // how long to turn on
+int offDuration = 5000; // how long to turn off
+
+bool isOn = false;
 
 void setup()
 {
+  Serial.begin(115200);
+  pinMode(pumpPin, OUTPUT);
+  prevMillis = millis();
 }
 
-void pumpControl()
+void schedulePump()
 {
-}
 
+  int schedule = Serial.parseInt(); // in seconds
+  if (schedule != 0)
+  {
+    offDuration = 1000 * schedule; // convert to ms
+    Serial.printf("New pump delay: %d seconds \n", schedule);
+  }
+}
 void loop()
 {
+  if (Serial.available() > 0)
+  {
+    schedulePump();
+  }
+
+  uint32_t t;
+  t = millis();
+  int currentMillis = millis();
+
+  if (!isOn)
+  {
+    if (currentMillis - prevMillis >= offDuration)
+    {
+      prevMillis = currentMillis;
+      isOn = true;
+      digitalWrite(pumpPin, HIGH);
+    }
+  }
+  else if (currentMillis - prevMillis >= onDuration)
+  {
+    prevMillis = currentMillis;
+    isOn = false;
+    digitalWrite(pumpPin, LOW);
+  }
 }
