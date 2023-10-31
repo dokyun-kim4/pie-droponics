dbbe4d542585a2fadf61de75df20894a2479f05e
Daeyoung Kim <corydykim@gmail.com>
2023-10-31 12:28:45 -0400

    Serial print temp and humidity

--- api_client/lib/APIClient/api_client.cpp
+++ api_client/lib/APIClient/api_client.cpp
@@ -1,35 +1,26 @@
 #include "api_client.h"
+#include "set_keys.h"
 
 String baseUrl = "https://joz9eef623.execute-api.us-east-2.amazonaws.com/dev/";
 
-void getRequest(String route, String apiKey)
-{
-    HTTPClient* http = createClient(route, apiKey);
-    // Make the request based on the request type
-    int httpCode;
-    httpCode = http->GET();
-    // Check the HTTP response code
-    if (httpCode > 0)
-    {
-        // Success
-        if (httpCode == HTTP_CODE_OK)
-        {
-            String payload = http->getString();
-            Serial.println("HTTP request successful. Response:");
-            Serial.println(payload);
-        }
-        else
-        {
-            Serial.print("HTTP request failed with error code: ");
-            Serial.println(httpCode);
-        }
-    }
-    else
-    {
-        Serial.println("HTTP request failed");
-    }
-    // End the request
-    http->end();
+String getRequest(String Url) {
+    String apiKey = getApiKey();
+    String ReqBody, ResponseStr;
+    WiFiClient Client;
+    HTTPClient Http;
+    int ResponseCode;
+    Serial.printf("Url: %s\n", Url.c_str());
+    Http.setTimeout(3000);
+    Http.begin(Url);
+    Http.addHeader("Content-Type", "application/json");
+    Http.addHeader("x-api-key", apiKey);
+    ResponseCode = Http.GET();
+    ResponseStr = Http.getString();
+    Serial.println("ResponseCode: " + String(ResponseCode));
+    Serial.println("ResponseStr: " + ResponseStr);
+    Http.end();
+
+  return ResponseStr;
 }
 
 void postRequest(String route, String apiKey, String requestPayload)
@@ -75,6 +66,23 @@ HTTPClient* createClient(String route, String apiKey)
 
     // Set the custom header for the API key
     http->addHeader("x-api-key", apiKey);
+    http->addHeader("Content-Type", "application/json");
 
     return http;
 }
+
+JsonObject parseResponse(String response)
+{
+    // Parse the response into a JSON object
+    DynamicJsonDocument doc(1024);
+    DeserializationError error = deserializeJson(doc, response);
+    if (error)
+    {
+        Serial.print("deserializeJson() failed: ");
+        Serial.println(error.c_str());
+        return JsonObject();
+    }
+    JsonObject body = doc.as<JsonObject>();
+    return body;
+}
+
--- api_client/src/main.cpp
+++ api_client/src/main.cpp
@@ -1,25 +1,49 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include "set_keys.h"
+#include "Adafruit_SHT4x.h"
 
+Adafruit_SHT4x sht4 = Adafruit_SHT4x();
 
-String ssid = "OLIN-DEVICES";
-
-void setup()
+void setup() 
 {
-  Serial.begin(115200);
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, getWifiKey());
-  while (WiFi.status() != WL_CONNECTED)
+  Serial.begin(9600);
+  while (!Serial)
+    delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit SHT4x test");
+  if (! sht4.begin()) 
   {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+    Serial.println("Couldn't find SHT4x");
+    while (1) delay(1);
   }
-  Serial.println("Connected to WiFi");
+  Serial.println("Found SHT4x sensor");
+  Serial.print("Serial number 0x");
+  Serial.println(sht4.readSerial(), HEX);
+
+  // You can have 3 different precisions, higher precision takes longer
+  sht4.setPrecision(SHT4X_HIGH_PRECISION);
+
+  // You can have 6 different heater settings
+  sht4.setHeater(SHT4X_NO_HEATER);
+  
 }
 
-void loop()
+
+void loop() 
 {
-  // Your code here
-}
+  sensors_event_t humidity, temp;
+  
+  uint32_t timestamp = millis();
+  sht4.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  timestamp = millis() - timestamp;
 
+  Serial.print("Temperature: "); 
+  Serial.print(temp.temperature); 
+  Serial.println(" degrees C");
+  Serial.print("Humidity: "); 
+  Serial.print(humidity.relative_humidity); 
+  Serial.println("% rH");
+
+  Serial.print("Read duration (ms): ");
+  Serial.println(timestamp);
+  delay(1000);
+}
