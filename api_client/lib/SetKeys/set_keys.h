02636dcf634592545930e06f2cca5a9ac6ad8d31
Daeyoung Kim <corydykim@gmail.com>
2023-11-01 20:02:32 -0400

    Functional pump, t-h sensor, scheduling

--- api_client/lib/SetKeys/set_keys.h
+++
@@ -1,6 +0,0 @@
-#include <Preferences.h>
-#include <Arduino.h>
-
-void setupKeys(String wifiKey, String apiKey);
-String getWifiKey();
-String getApiKey();
--- sprint-1/src/main.cpp
+++ sprint-1/src/main.cpp
@@ -1,5 +1,11 @@
 #include <Arduino.h>
+#include "Adafruit_SHT4x.h"
 #define pumpPin 10
+
+void getTempHumid();
+void schedulePump();
+
+Adafruit_SHT4x sht4 = Adafruit_SHT4x();
 
 uint32_t prevMillis;
 int onDuration = 2000;  // how long to turn on
@@ -10,23 +16,33 @@ bool isOn = false;
 void setup()
 {
   Serial.begin(115200);
+  while (!Serial)
+    delay(10);
+  
+  // Setup for pump schedule
   pinMode(pumpPin, OUTPUT);
   prevMillis = millis();
-}
 
-void schedulePump()
-{
-
-  int schedule = Serial.parseInt(); // in seconds
-  if (schedule != 0)
+  // Setup for temp/humidity sensor
+  if (! sht4.begin()) 
   {
-    offDuration = 1000 * schedule; // convert to ms
-    Serial.printf("New pump delay: %d seconds \n", schedule);
+    Serial.println("Couldn't find SHT4x sensor");
+    while (1) delay(1);
   }
+  Serial.println("Found SHT4x sensor");
+  Serial.print("Serial number 0x");
+  Serial.println(sht4.readSerial(), HEX);
+
+  // You can have 3 different precisions, higher precision takes longer
+  sht4.setPrecision(SHT4X_HIGH_PRECISION);
+
+  // You can have 6 different heater settings
+  sht4.setHeater(SHT4X_NO_HEATER);
 }
+
 void loop()
 {
-  if (Serial.available() > 0)
+  if (Serial.available())
   {
     schedulePump();
   }
@@ -42,6 +58,7 @@ void loop()
       prevMillis = currentMillis;
       isOn = true;
       digitalWrite(pumpPin, HIGH);
+      getTempHumid();
     }
   }
   else if (currentMillis - prevMillis >= onDuration)
@@ -50,4 +67,26 @@ void loop()
     isOn = false;
     digitalWrite(pumpPin, LOW);
   }
+}
+
+void schedulePump()
+{
+  int schedule = Serial.parseInt(); // in seconds
+  if (schedule != 0)
+  {
+    offDuration = 1000 * schedule; // convert to ms
+    Serial.printf("New pump delay: %d seconds \n", schedule);
+  }
+}
+
+void getTempHumid()
+{
+  sensors_event_t humidity, temp;
+  sht4.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  Serial.print("Temperature: "); 
+  Serial.print(temp.temperature); 
+  Serial.println(" degrees C");
+  Serial.print("Humidity: "); 
+  Serial.print(humidity.relative_humidity); 
+  Serial.println("% rH");
 }
