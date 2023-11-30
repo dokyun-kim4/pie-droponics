71bad8874746ae68836dcc1db33259d38b984db0
Dokyun Kim <donny5557@gmail.com>
2023-11-30 16:55:30 -0500

    aws integration

--- schedule/src/main.cpp
+++ schedule/src/main.cpp
@@ -59,65 +59,65 @@ const char *ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = -18000;
 const int daylightOffset_sec = 3600;
 // -------------- Sensor setup ----------------------//
-// void setupSHT40()
-// {
-//   // Setup for temp/humidity sensor
-//   if (!sht4.begin())
-//   {
-//     Serial.println("Couldn't find SHT4x sensor");
-//     while (1)
-//       delay(1);
-//   }
-//   Serial.println("Found SHT4x sensor");
-//   Serial.print("Serial number 0x");
-//   Serial.println(sht4.readSerial(), HEX);
+void setupSHT40()
+{
+  // Setup for temp/humidity sensor
+  if (!sht4.begin())
+  {
+    Serial.println("Couldn't find SHT4x sensor");
+    while (1)
+      delay(1);
+  }
+  Serial.println("Found SHT4x sensor");
+  Serial.print("Serial number 0x");
+  Serial.println(sht4.readSerial(), HEX);
 
-//   // You can have 3 different precisions, higher precision takes longer
-//   sht4.setPrecision(SHT4X_HIGH_PRECISION);
+  // You can have 3 different precisions, higher precision takes longer
+  sht4.setPrecision(SHT4X_HIGH_PRECISION);
 
-//   // You can have 6 different heater settings
-//   sht4.setHeater(SHT4X_NO_HEATER);
-// }
+  // You can have 6 different heater settings
+  sht4.setHeater(SHT4X_NO_HEATER);
+}
 
-// // Create an alias for temperature and humidity readings
-// typedef std::pair<float, float> TempHumidReading;
+// Create an alias for temperature and humidity readings
+typedef std::pair<float, float> TempHumidReading;
 
-// // Function to get temperature and humidity readings
-// TempHumidReading getTempHumid()
-// {
-//   if (!sht4.begin())
-//   {
-//     Serial.println("SHT4x sensor not setup or disconnected!");
-//     while (1)
-//       delay(1);
-//   }
+// Function to get temperature and humidity readings
+TempHumidReading getTempHumid()
+{
+  if (!sht4.begin())
+  {
+    Serial.println("SHT4x sensor not setup or disconnected!");
+    while (1)
+      delay(1);
+  }
 
-//   sensors_event_t humidity, temp;
-//   sht4.getEvent(&humidity, &temp); // Populate temp and humidity objects with fresh data
-//   return {temp.temperature, humidity.relative_humidity};
-// }
+  sensors_event_t humidity, temp;
+  sht4.getEvent(&humidity, &temp); // Populate temp and humidity objects with fresh data
+  return {temp.temperature, humidity.relative_humidity};
+}
 
-// void setupBH1750()
-// {
-//   bool avail = BH1750.begin(BH1750_TO_GROUND); // BH1750_TO_GROUND or BH1750_TO_VCC for addr pin
-//   if (!avail)
-//   {
-//     Serial.println("No BH1750 sensor found!");
-//     while (1)
-//       delay(1);
-//   }
+void setupBH1750()
+{
+  bool avail = BH1750.begin(BH1750_TO_GROUND); // BH1750_TO_GROUND or BH1750_TO_VCC for addr pin
+  if (!avail)
+  {
+    Serial.println("No BH1750 sensor found!");
+    while (1)
+      delay(1);
+  }
 
-//   // BH1750.calibrateTiming();  //uncomment this line if you want to speed up your sensor
-//   Serial.printf("conversion time: %dms\n", BH1750.getMtregTime());
-//   BH1750.start(); // start the first measurement in setup
-// }
+  // BH1750.calibrateTiming();  //uncomment this line if you want to speed up your sensor
+  Serial.printf("conversion time: %dms\n", BH1750.getMtregTime());
+  BH1750.start(); // start the first measurement in setup
+}
 
-// float getLuminance()
-// {
-//   BH1750.start();
-//   float lux = BH1750.getLux();
-//   return lux;
-// }
+float getLuminance()
+{
+  BH1750.start();
+  float lux = BH1750.getLux();
+  return lux;
+}
 
 // -------------------- AWS functions ----------------//
 void messageHandler(String &topic, String &payload)
@@ -227,33 +227,35 @@ void connectAWS()
   Serial.println("AWS IoT Connected!");
 }
 
-// void publishMessage(String sensorType, StaticJsonDocument<200> doc, char *payload)
-// {
-//   if (sensorType == "temperature")
-//   {
-//     TempHumidReading tempHumidReading = getTempHumid();
-//     doc["value"] = tempHumidReading.first;
-//     serializeJson(doc, jsonBuffer);
-//     client.publish(String(AWS_IOT_PUBLISH_TOPIC) + String("temperature"), jsonBuffer);
-//     Serial.println(doc.as<String>());
-//   }
-//   else if (sensorType == "humidity")
-//   {
-//     TempHumidReading tempHumidReading = getTempHumid();
-//     doc["value"] = tempHumidReading.second;
-//     serializeJson(doc, jsonBuffer);
-//     client.publish(String(AWS_IOT_PUBLISH_TOPIC) + String("humidity"), jsonBuffer);
-//     Serial.println(doc.as<String>());
-//   }
-//   else if (sensorType == "luminance")
-//   {
-//     float lux = getLuminance();
-//     doc["value"] = lux;
-//     serializeJson(doc, jsonBuffer);
-//     client.publish(String(AWS_IOT_PUBLISH_TOPIC) + String("luminance"), jsonBuffer);
-//     Serial.println(doc.as<String>());
-//   }
-// }
+void publishMessage(String sensorType, StaticJsonDocument<200> doc)
+{
+  if (sensorType == "temperature")
+  {
+    TempHumidReading tempHumidReading = getTempHumid();
+    doc["value"] = tempHumidReading.first;
+    serializeJson(doc, jsonBuffer);
+    client.publish(String(AWS_IOT_PUBLISH_TOPIC) + String("temperature"), jsonBuffer);
+    Serial.println(doc.as<String>());
+  }
+  else if (sensorType == "humidity")
+  {
+    TempHumidReading tempHumidReading = getTempHumid();
+    doc["value"] = tempHumidReading.second;
+    serializeJson(doc, jsonBuffer);
+    client.publish(String(AWS_IOT_PUBLISH_TOPIC) + String("humidity"), jsonBuffer);
+    Serial.println(doc.as<String>());
+  }
+  else if (sensorType == "luminance")
+  {
+    float lux = getLuminance();
+    doc["value"] = lux;
+    serializeJson(doc, jsonBuffer);
+    client.publish(String(AWS_IOT_PUBLISH_TOPIC) + String("luminance"), jsonBuffer);
+    Serial.println(doc.as<String>());
+  }
+  doc.clear();                               // clear the JSON document for reuse
+  memset(jsonBuffer, 0, sizeof(jsonBuffer)); // clear the char array
+}
 
 // -------------------- ESP control functions ------------ //
 void setup()
@@ -466,11 +468,14 @@ void runLight()
 
 void loop()
 {
+  String sensorTypes[] = {"temperature", "humidity", "luminance"};
+  publishMessage(sensorTypes[0], doc);
+  publishMessage(sensorTypes[1], doc);
+  publishMessage(sensorTypes[2], doc);
+
   client.loop();
-  delay(100);
-  // runWater();
-  // runNutrient();
-  // delay(1000);
-  // printSensor();
-  // runLight();
+  runWater();
+  runNutrient();
+  delay(1000);
+  runLight();
 }
